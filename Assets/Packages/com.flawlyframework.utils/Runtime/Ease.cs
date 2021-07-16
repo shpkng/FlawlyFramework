10b29e0e1e47d600bdc1bd0012bf4a5183c8d6cd
吴晨杨 <wuchenyang@dxx.cn>
2021-07-16 20:23:46 +0800

    增加Ease库

---
+++ Assets/Packages/com.flawlyframework.utils/Runtime/Ease.cs
@@ -0,0 +1,160 @@
+﻿// Author: wuchenyang(shpkng@gmail.com)
+
+// Math functions are from https://easings.net/cn. 
+// Check the website for visualised illustrations.
+
+namespace FF.Utils
+{
+    public static class Ease
+    {
+        public static float InSine(float t)
+        {
+            return 0;
+        }
+
+        public static float OutSine(float t)
+        {
+            return 0;
+        }
+
+        public static float InOutSine(float t)
+        {
+            return 0;
+        }
+
+        public static float InCubic(float t)
+        {
+            return 0;
+        }
+
+        public static float OutCubic(float t)
+        {
+            return 0;
+        }
+
+        public static float InOutCubic(float t)
+        {
+            return 0;
+        }
+
+        public static float InQuint(float t)
+        {
+            return 0;
+        }
+
+        public static float OutQuint(float t)
+        {
+            return 0;
+        }
+
+        public static float InOutQuint(float t)
+        {
+            return 0;
+        }
+
+        public static float InCircles(float t)
+        {
+            return 0;
+        }
+
+        public static float OutCircle(float t)
+        {
+            return 0;
+        }
+
+        public static float InOutCircle(float t)
+        {
+            return 0;
+        }
+
+        public static float InElastic(float t)
+        {
+            return 0;
+        }
+
+        public static float OutElastic(float t)
+        {
+            return 0;
+        }
+
+        public static float InOutElastic(float t)
+        {
+            return 0;
+        }
+
+        public static float InQuad(float t)
+        {
+            return 0;
+        }
+
+        public static float OutQuad(float t)
+        {
+            return 0;
+        }
+
+        public static float InOutQuad(float t)
+        {
+            return 0;
+        }
+
+        public static float InQuart(float t)
+        {
+            return 0;
+        }
+
+        public static float OutQuart(float t)
+        {
+            return 0;
+        }
+
+        public static float InOutQuart(float t)
+        {
+            return 0;
+        }
+
+        public static float InExpo(float t)
+        {
+            return 0;
+        }
+
+        public static float OutExpo(float t)
+        {
+            return 0;
+        }
+
+        public static float InOutExpo(float t)
+        {
+            return 0;
+        }
+
+        public static float InBack(float t)
+        {
+            return 0;
+        }
+
+        public static float OutBack(float t)
+        {
+            return 0;
+        }
+
+        public static float InOutBack(float t)
+        {
+            return 0;
+        }
+
+        public static float InBounce(float t)
+        {
+            return 0;
+        }
+
+        public static float OutBounce(float t)
+        {
+            return 0;
+        }
+
+        public static float InOutBounce(float t)
+        {
+            return 0;
+        }
+    }
+}
